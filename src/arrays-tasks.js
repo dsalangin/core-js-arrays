9c730fd4774c65fd6d199637c7b84b6af6db5d16
dmitriy.salangin <dmitriy.salangin@simpleone.ru>
2024-02-06 22:45:00 +0300

    feat: solve sum array task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,14 +37,11 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
-  // const result = [];
-  // const longerArr = arr1.length > arr2.length ? arr1 : arr2;
-  // longerArr.forEach((item, index) => {
-  //   result.push((arr1[index] || 0) + (arr2[index] || 0));
-  // });
-  // return result;
+function sumArrays(arr1, arr2) {
+  const longerArr = arr1.length > arr2.length ? arr1 : arr2;
+  return longerArr.map((_, index) => {
+    return (arr1[index] || 0) + (arr2[index] || 0);
+  });
 }
 
 /**
